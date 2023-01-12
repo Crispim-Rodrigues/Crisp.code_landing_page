11bc8f1a03a3659f33f04919d5113e525bdcfe28
unknown <crispim.rodrigues.oficial@gmail.com>
2023-01-12 01:58:55 -0300

    Fim carrocel

--- src/conponents/Coments.js
+++ src/conponents/Coments.js
@@ -1,6 +1,34 @@
+import { useState, useEffect, useRef } from 'react';
 import '../App.css';
+import { motion } from 'framer-motion';
+import perfil from '../images/perfil.jpg';
 
 function Coments(){
+    const carousel = useRef();
+    const [width, setWidth] = useState(0);
+
+    useEffect(()=> {
+        setWidth(carousel.current?.scrollWidth - carousel.current?.offsetWidth)
+    },[])
+
+
+
+    const items = [];
+    for (let i = 0; i < 3; i++) {
+        items.push(
+        <motion.div key={i} className='single_carrosel'
+            
+        >
+            <h2>Crispim Rodrigues<img src={perfil} alt="" /><p>Full-Stack Developer</p></h2> 
+            <p>
+            Proin sagittis, risus ut tempus condimentum, urna est volutpat augue, 
+            a placerat ligula magna tincidunt sapien.
+            Ut tempus pulvinar ante, at euismod orci.
+            </p>
+        </motion.div>
+        );
+    }
+
     return(
         <section className='coments_main'>
                 <div className="w50">
@@ -16,7 +44,19 @@ function Coments(){
                 </div>
                 <div className="w50">
                     <div className='back_right'>
-                        
+                        <motion.div 
+                        className='carrosel' 
+                        whileTap={{ cursor: 'grabbing'}}
+                        ref={carousel}
+                        >
+                            <motion.div
+                            className='Inner'
+                            drag="x"
+                            dragConstraints={{ right: 0, left: -width}}
+                            >
+                             {items} 
+                            </motion.div>     
+                        </motion.div>
                     </div>    
                 </div>
         </section>
