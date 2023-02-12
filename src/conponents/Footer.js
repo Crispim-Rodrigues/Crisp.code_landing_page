626e5ca121288b6b3dba0e531a54b8025f40ee62
unknown <crispim.rodrigues.oficial@gmail.com>
2023-02-12 00:40:34 -0300

    retoques css

--- src/conponents/Footer.js
+++ src/conponents/Footer.js
@@ -3,18 +3,10 @@ import "../App.css";
 function Footer(){
     return(
         <footer>
-            <div className="center">
-                <div className="w50">
-                    <div>
-                        <h4>{'crispim.rodrigues.oficial@gmail.com / (13) 996011881'}</h4>
-                    </div>
-                </div>
-                <div className="w50">
-                    <div>
-                        <h4>Feito por pessoa e para pessoa</h4>
-                    </div>
-                </div>
-            </div>
+            
+            <span>{'crispim.rodrigues.oficial@gmail.com / (13) 996011881'}</span>
+            <span>Feito por pessoa e para pessoa</span>
+            
         </footer>
     )
 }
